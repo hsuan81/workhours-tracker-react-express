daf1796d0b2ceadb10dbfffbed0d9d2a17b0a54e
hsuanyc <61041593+hsuan81@users.noreply.github.com>
2025-07-08 11:53:58 +0800

    Added employee dashboard page and today summary and monthly overview components

---
+++ apps/frontend/src/components/MonthlyOverviewCard.tsx
@@ -0,0 +1,37 @@
+// src/components/MonthlyOverviewCard.tsx
+import { useEffect, useState } from "react"
+import type { MonthlyOverview } from "../types/api"
+
+export function MonthlyOverviewCard() {
+    const [overview, setOverview] = useState<MonthlyOverview | null>(null)
+    // const [loading, setLoading] = useState(true)
+    const month = new Date().getMonth() + 1 // 1-12
+
+    useEffect(() => {
+        const mock: MonthlyOverview = {
+            year: 2025,
+            month: month,
+            daysWorked: 20,
+            regularHours: 160,
+            overtimeHours: 10,
+            overtimePay: 300,
+            regularHourLimit: 160,
+            overtimeLimit: 20
+        }
+
+        setTimeout(() => setOverview(mock), 300)
+        // fetch('/api/entries/overview?month=' + month).then(res => res.json()).then(setOverview)
+    }, [])
+    
+    if (!overview) {
+        return <div>Loading monthly overview...</div>
+    }
+
+    return <div className="p-4 border rounded-xl shadow w-full max-w-full">
+        <h2 className="text-lg font-semibold mb-2">Monthly Overview</h2>
+        <p>Days Worked: {overview.daysWorked}</p>
+        <p>Regular Hours: {overview.regularHours}/{overview.regularHourLimit}</p>
+        <p>Overtime: {overview.overtimeHours} / {overview.overtimeLimit}</p>
+        <p>Est. Overtime Pay: {overview.overtimePay}</p>
+        </div>
+}
---
+++ apps/frontend/src/components/TodaySummaryCard.tsx
@@ -0,0 +1,37 @@
+// src/components/TodaySummaryCard.tsx
+import { useEffect, useState } from 'react'
+import type { DailySummary } from '../types/api'
+
+export function TodaySummaryCard() {
+  const [summary, setSummary] = useState<DailySummary | null>(null)
+
+  useEffect(() => {
+    const mock: DailySummary = {
+        date: '2025-07-07',
+        totalHours: 8.5,
+        regularHours: 8,
+        overtimeHours: 0.5,
+        pay: 250,
+        projects: [
+          { id: '1', name: 'Project A', hours: 5 },
+          { id: '2', name: 'Project B', hours: 3.5 },
+        ],
+      }
+    
+    setTimeout(() => setSummary(mock), 300)
+    // fetch('/api/entries/summary')
+    //   .then(res => res.json())
+    //   .then(setSummary)
+  }, [])
+
+  if (!summary) return <div>Loading today's summary...</div>
+
+  return (
+    <div className="p-4 border rounded-xl shadow w-full max-w-full">
+      <h2 className="text-lg font-semibold mb-2">Today's Summary</h2>
+      <p>Hours: {summary.totalHours}/8 + {summary.overtimeHours}</p>
+      <p>Projects: {summary.projects.map(p => {return p.name + ' - ' + p.hours + 'hrs'}).join(', ')}</p>
+      <p>Status: 🟡</p>
+    </div>
+  )
+}
