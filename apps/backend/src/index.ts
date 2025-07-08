f8002b2806180088b95e314c5a1998c82920d10b
hsuanyc <61041593+hsuan81@users.noreply.github.com>
2025-07-08 11:14:03 +0800

    Set up frontend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,18 +1,29 @@
 import express, { Request, Response } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import { PrismaClient } from "./generated/prisma/index.js"
 
-dotenv.config({ path: "../../.env" }) // load environment variables from .env file
+dotenv.config({ path: "../.env" }) // load environment variables from .env file
 
 const app = express()
-app.use(cors()) // Enable CORS for all routes
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    // credentials: true, // If using cookie or auth header, add this line
+  })
+) // Enable CORS for a limited set of origins
 app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello from backend")
+  res.json({ message: "Hello from backend" })
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.BACKEND_PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
+
+const prisma = new PrismaClient()
+// 測試 CRUD 例如：
+const users = await prisma.user.findMany()
+console.log("Connecting prisma", users)
---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from 'react';
+
+import EmployeeDashboard from './pages/EmployeeDashboard'
+
+export default function App() {
+  return <EmployeeDashboard />
+}
