3d4a0a20a0196d2e36a5835e17ff901764022967
hsuanyc <61041593+hsuan81@users.noreply.github.com>
2025-07-18 19:21:20 +0800

    Added custom color css and modified style

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,20 +1,20 @@
 // App.tsx
-import { useState } from "react";
-import EmployeeDashboard from "./pages/EmployeeDashboard";
-import LogHoursPage from "./pages/LogHoursPage";
+import { useState } from "react"
+import EmployeeDashboard from "./pages/EmployeeDashboard"
+import LogHoursPage from "./pages/LogHoursPage"
 
 function App() {
-  const [page, setPage] = useState("dashboard");
+  const [page, setPage] = useState("dashboard")
 
   return (
     <div>
-      <nav className="space-x-4 p-4 bg-gray-100">
+      <nav className="space-x-4 p-4 bg-custom-gray">
         <button onClick={() => setPage("dashboard")}>Dashboard</button>
         <button onClick={() => setPage("log")}>Log Hours</button>
       </nav>
       {page === "dashboard" && <EmployeeDashboard />}
       {page === "log" && <LogHoursPage />}
     </div>
-  );
+  )
 }
-export default App;
+export default App
--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-black": "#181d2a",
+        "custom-gray": "#e8ebed",
+        "custom-blue": "#748efe",
+        "custom-white": "#ffffff",
+        "custom-red": "#ee4b2b",
+      },
+    },
   },
   plugins: [],
 }
-
